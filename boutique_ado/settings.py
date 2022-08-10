1276acd4f129349b5638deaed69ddc74229dc34e
Squirkey89 <squirke89@gmail.com>
2022-08-10 11:52:50 +0000

    Add STATIC ROOT

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-g5*zo%_u3c(vnb*god)0v_@g(@&-lued0vgeim8fat$2j0l-gr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['boutique-ado89.herokuapp.com', 'localhost']
 
@@ -172,6 +172,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
