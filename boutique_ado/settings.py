899199cdc0f53d21c011d1204f767c7386279e48
Squirkey89 <squirke89@gmail.com>
2022-08-11 13:26:48 +0000

    Added cache control

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -182,7 +182,13 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    #Bucket Config
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'CacheControl': 'max-age=94608000',
+    }    
+
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'boutique-ado89'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACESS_KEY_ID = os.environ.get('AWS_ACESS_KEY_ID')
@@ -194,7 +200,6 @@ if 'USE_AWS' in os.environ:
     STATICFILES_LOCATION = 'static'
     STATIC_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
-
 
     # Override Static and media URLs in production
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
