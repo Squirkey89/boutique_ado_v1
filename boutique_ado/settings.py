633772aa264757d32b489675386b1f5f1972faa1
Squirkey89 <squirke89@gmail.com>
2022-08-11 20:20:48 +0000

    Update settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -186,23 +186,23 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    # Cache Control
+    # Cache control
     AWS_S3_OBJECT_PARAMETERS = {
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
-    }    
+    }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado89'
-    AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_ACESS_KEY_ID = os.environ.get('AWS_ACESS_KEY_ID')
-    AWS_SECRET_ACESS_KEY = os.environ.get('AWS_SECRET_ACESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = 'ckz8780-boutique-ado'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-    # Static media and files
+    # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
     STATICFILES_LOCATION = 'static'
-    STATIC_FILE_STORAGE = 'custom_storages.MediaStorage'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
     MEDIAFILES_LOCATION = 'media'
 
     # Override Static and media URLs in production
