805bd43d8eb22f279ba546a84eada608a651efa2
Squirkey89 <squirke89@gmail.com>
2022-08-11 12:29:30 +0000

    deployment part 5

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -179,6 +180,27 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    #Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'boutique-ado89'
+    AWS_S3_REGION_NAME = 'eu-west-1'
+    AWS_ACESS_KEY_ID = os.environ.get('AWS_ACESS_KEY_ID')
+    AWS_SECRET_ACESS_KEY = os.environ.get('AWS_SECRET_ACESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    # Static media and files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    STATIC_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+
+    # Override Static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}'
+
+
 
 #Stripe
 FREE_DELIVERY_THRESHOLD = 50
