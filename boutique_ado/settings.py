4d4362fbb9337021f1d84464eef6d6c99167176f
Squirkey89 <squirke89@gmail.com>
2022-08-12 10:27:04 +0000

    Fix typo

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '+65k^3c6e*4!oz*%$f4&&sxpg1!nugzi@jh_=$f^f31qbx$nz0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
