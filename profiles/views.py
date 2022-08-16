6d854a095952209ee4c48fc5964994ea1224ef2b
Squirkey89 <squirke89@gmail.com>
2022-08-16 10:21:38 +0000

    Addedsitemap abd robots.txt

--- profiles/views.py
+++ profiles/views.py
@@ -7,17 +7,16 @@ from .forms import UserProfileForm
 from checkout.models import Order
 
 
-
 @login_required
 def profile(request):
-    """ Display the user's profile"""
+    """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
 
     if request.method == 'POST':
         form = UserProfileForm(request.POST, instance=profile)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated sucessfully')
+            messages.success(request, 'Profile updated successfully')
         else:
             messages.error(request, 'Update failed. Please ensure the form is valid.')
     else:
@@ -33,12 +32,13 @@ def profile(request):
 
     return render(request, template, context)
 
+
 def order_history(request, order_number):
     order = get_object_or_404(Order, order_number=order_number)
 
     messages.info(request, (
-        f'This is a confirmation for order number{order_number}.'
-        'A confirmation email was sent on the order date'
+        f'This is a past confirmation for order number {order_number}. '
+        'A confirmation email was sent on the order date.'
     ))
 
     template = 'checkout/checkout_success.html'
