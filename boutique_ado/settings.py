2b337936e8a16aaeec176615fd98eb05b276cf0f
Squirkey89 <squirke89@gmail.com>
2022-08-18 17:15:14 +0000

    changed debug to false

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '+65k^3c6e*4!oz*%$f4&&sxpg1!nugzi@jh_=
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['boutique-ado89.herokuapp.com', 'localhost']
 
